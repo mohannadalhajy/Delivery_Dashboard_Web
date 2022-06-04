8a76ea6d7a253b2553ef9be3b5bf319bcd6f70cf
MetaGols <mohannad.alhajy@gmail.com>
2022-06-05 02:23:54 +0400

    edits

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ store.subscribe(() => console.log(store.getState()));
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register(`${process.env.PUBLIC_URL + '/firebase-messaging-sw.js'}`)
+    .register("./firebase-messaging-sw.js")
     .then(function(registration) {
       console.log("Registration successful, scope is:", registration.scope);
     })
