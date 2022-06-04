8257b1e7bf97e39d7f00517973fbac5581b71ef4
MetaGols <mohannad.alhajy@gmail.com>
2022-06-05 02:50:49 +0400

    edits

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -20,23 +20,32 @@ firebase.initializeApp({
 // messages.
 const messaging = firebase.messaging();
 
-// messaging.setBackgroundMessageHandler(function(payload) {
-//   const promiseChain = clients
-//        .matchAll({
-//             type: "window",
-//             includeUncontrolled: true,
-//        })
-//        .then((windowClients) => {
-//             for (let i = 0; i < windowClients.length; i++) {
-//                  const windowClient = windowClients[i];
-//                  windowClient.postMessage(payload);
-//             }
-//        })
-//        .then(() => {
-//             return registration.showNotification("my notification title");
-//        });
-//   return promiseChain;
-// });
-// self.addEventListener("notificationclick", function(event) {
-//   console.log(event);
-// });
+messaging.setBackgroundMessageHandler(function(payload) {
+  const promiseChain = clients
+       .matchAll({
+            type: "window",
+            includeUncontrolled: true,
+       })
+       .then((windowClients) => {
+            for (let i = 0; i < windowClients.length; i++) {
+                 const windowClient = windowClients[i];
+                 windowClient.postMessage(payload);
+            }
+       })
+       .then(() => {
+            return registration.showNotification("my notification title");
+       });
+  return promiseChain;
+});
+firebase.addEventListener("notificationclick", function(event) {
+  console.log(event);
+});
+firebase.addEventListener("message", function(event) {
+  console.log(event);
+});
+messaging.addEventListener("notificationclick", function(event) {
+  console.log(event);
+});
+messaging.addEventListener("message", function(event) {
+  console.log(event);
+});
