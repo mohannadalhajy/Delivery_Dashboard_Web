d5296f17a597d954fd207ceb3d2dd3c5eba656e1
MetaGols <mohannad.alhajy@gmail.com>
2022-06-04 20:21:47 +0400

    edits

--- src/redux/User/Actions.js
+++ src/redux/User/Actions.js
@@ -10,8 +10,8 @@ import {
     FETCH_INIT_EDIT_PROFILE,
     FETCH_INIT_PROFILE
 } from "./ActionTypes";
-import messaging from '../../FirebaseConf'
-import { getToken } from "firebase/messaging";
+// import messaging from '../../FirebaseConf'
+// import { getToken } from "firebase/messaging";
 
 const clientAuth = require('./API');
 
@@ -38,10 +38,10 @@ export const loginFailureAction = (error) => {
 export const login = body => async (dispatch, getState) => {
     dispatch(loginRequestAction());
     const firebaseToken = ""
-     await getToken(
-        messaging, 
-        { vapidKey: 'BGKxsZKcTHGI08qArtUOAdlxa12Og_DpMJ6JjKnWQIGuH-i7sKpMGAbj1icCAUJUlEln_b-W0-8IvPEqJwxyHyk' }
-        )
+    //  await getToken(
+    //     messaging, 
+    //     { vapidKey: 'BGKxsZKcTHGI08qArtUOAdlxa12Og_DpMJ6JjKnWQIGuH-i7sKpMGAbj1icCAUJUlEln_b-W0-8IvPEqJwxyHyk' }
+    //     )
     ///2NpOjFNyHGNSCTithmeXIG-JGsZkxIfsA-7NUXQ28Zs
     console.log("firebaseToken",firebaseToken)
     const promise = clientAuth.login({...body, firebaseToken});
