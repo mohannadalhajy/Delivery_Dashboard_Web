86361f8f884d0c5d07c1fbff8deba295623a9495
MetaGols <mohannad.alhajy@gmail.com>
2022-06-05 03:11:46 +0400

    edits

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store';
 import { Provider } from 'react-redux';
-// import { registerServiceWorker } from './serviceWorker';
+import { registerServiceWorker } from './serviceWorker';
 import messaging from "./FirebaseConf";
 
 messaging.onMessage(payload=>{
@@ -38,6 +38,7 @@ ReactDOM.render(
     </React.StrictMode>,
   document.getElementById('root')
 );
+registerServiceWorker();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
