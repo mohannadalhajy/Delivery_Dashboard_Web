d2ad86e0603bb77bb3215aef6213c89506e984cb
MetaGols <mohannad.alhajy@gmail.com>
2022-06-05 10:11:39 +0400

    edits

--- src/FirebaseConf.js
+++ src/FirebaseConf.js
@@ -1,13 +1,21 @@
 import firebase from 'firebase/compat/app';
 import "firebase/compat/messaging"
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../firebase-messaging-sw.js')
+    .then(function (registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    }).catch(function (err) {
+      console.log('Service worker registration failed, error:', err);
+    });
+}
 const firebaseConfig = {
-    apiKey: "AIzaSyDVFvurJK6PyxOgj9jS54HDa6lvSbUlJfI",
-    authDomain: "sinbad-delivery.firebaseapp.com",
-    projectId: "sinbad-delivery",
-    storageBucket: "sinbad-delivery.appspot.com",
-    messagingSenderId: "202365528516",
-    appId: "1:202365528516:android:9af69bce9902c6350edc91",
-  };
+  apiKey: "AIzaSyDVFvurJK6PyxOgj9jS54HDa6lvSbUlJfI",
+  authDomain: "sinbad-delivery.firebaseapp.com",
+  projectId: "sinbad-delivery",
+  storageBucket: "sinbad-delivery.appspot.com",
+  messagingSenderId: "202365528516",
+  appId: "1:202365528516:android:9af69bce9902c6350edc91",
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
