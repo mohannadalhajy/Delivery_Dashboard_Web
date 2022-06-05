ea8ed9c979e69bb670224a5581597d6c2c186eed
MetaGols <mohannad.alhajy@gmail.com>
2022-06-05 10:16:57 +0400

    edits

--- src/FirebaseConf.js
+++ src/FirebaseConf.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/compat/app';
 import "firebase/compat/messaging"
+import "@firebase/messaging"
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('../firebase-messaging-sw.js')
     .then(function (registration) {
